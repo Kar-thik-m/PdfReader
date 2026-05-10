1dabccdd7ca64ab7662e27fed5e3aa4f044f5541
Kar-thik-m <Sparrowkarthik007@gmail.com>
2026-05-10 17:31:41 +0530

    Resolved Cors Error

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,11 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: "*", // Allows all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 app.use("/api/words", WordRouter);
